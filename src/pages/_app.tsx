db29a467531c4fe3a286d85a663a4ff42e367d3f
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-04-16 10:48:07 +0200

    feat: add README

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,24 +6,6 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Script strategy="lazyOnload" id="iubenda-privacy">
-				{`
-        (function
-          (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
-      	`}
-			</Script>
-			<Script strategy="lazyOnload" id="iubenda-cookie-banner">
-				{`
-				var _iub = _iub || [];
-				_iub.csConfiguration = {"floatingPreferencesButtonDisplay":"bottom-right","lang":"en","perPurposeConsent":true,"siteId":3081551,"whitelabel":false,"cookiePolicyId":25968168, "banner":{ "acceptButtonDisplay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-top-center" }};
-				`}
-			</Script>
-			<Script
-				strategy="lazyOnload"
-				id="iubenda-cookie"
-				src="//cdn.iubenda.com/cs/iubenda_cs.js"
-				async
-			/>
 			<Analytics />
 			<Component {...pageProps} />
 		</>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,42 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	return (
+		<Html lang="en">
+			<Head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-P5CPX5H');`,
-          }}
-        />
-      </Head>
+					}}
+				/>
 
-      <body>
-        <Main />
-        <NextScript />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5CPX5H" height="0" width="0" style="display: none; visibility: hidden;" />`,
-          }}
-        />
-      </body>
-    </Html>
-  );
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `var _iub = _iub || [];
+_iub.csConfiguration = {"floatingPreferencesButtonDisplay":"bottom-right","lang":"it","perPurposeConsent":true,"siteId":3081551,"whitelabel":false,"cookiePolicyId":34668176, "banner":{ "acceptButtonDisplay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-top-center" }};`,
+					}}
+				/>
+				<script
+					type="text/javascript"
+					src="//cdn.iubenda.com/cs/iubenda_cs.js"
+					charSet="UTF-8"
+					async
+				></script>
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5CPX5H" height="0" width="0" style="display: none; visibility: hidden;" />`,
+					}}
+				/>
+			</body>
+		</Html>
+	);
 }
