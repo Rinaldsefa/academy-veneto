50bb559ee0bf30c5c4a2d92f1bed15c6ffc0cd61
SamuelePiazzesi <samuele.piazzesi@octoenergy.com>
2024-11-27 09:23:14 +0100

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import { useStepsStore } from "@/state/steps";
 import { useRouter } from "next/router";
 import Map from "@/components/map";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export const aileron = localFont({
   src: [
@@ -65,29 +66,25 @@ export default function Home() {
 
       <Header />
 
-      import { XMarkIcon } from '@heroicons/react/20/solid'
-
-export default function Example() {
-  return (
-    <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-      <p className="text-sm/6 text-white">
-        <a href="#">
-          <strong className="font-semibold">GeneriCon 2023</strong>
-          <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
-            <circle r={1} cx={1} cy={1} />
-          </svg>
-          Join us in Denver from June 7 – 9 to see what’s coming next&nbsp;<span aria-hidden="true">&rarr;</span>
-        </a>
-      </p>
-      <div className="flex flex-1 justify-end">
-        <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
-          <span className="sr-only">Dismiss</span>
-          <XMarkIcon aria-hidden="true" className="size-5 text-white" />
-        </button>
+      <div className="flex items-center gap-x-6 text-center bg-primary px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        <p className="text-sm/6 text-white text-center">
+          <Link href="/job-orienta">
+            <strong className="font-semibold">Its academy Veneto </strong>
+            al job orienta
+            <span aria-hidden="true">&rarr;</span>
+            <strong className="font-bold"> {"  "}Scopri di più</strong>
+          </Link>
+        </p>
+        <div className="flex flex-1 justify-end">
+          <button
+            type="button"
+            className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+          >
+            <span className="sr-only">Dismiss</span>
+            <XMarkIcon aria-hidden="true" className="size-5 text-white" />
+          </button>
+        </div>
       </div>
-    </div>
-  )
-}
 
       <div className={`bg-white ${aileron.className}`}>
         <main
